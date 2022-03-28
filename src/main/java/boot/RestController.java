62814bcde23f5dc02be7c7f4de95180cc8ff7703
kirill <kdgigante@gmail.com>
2022-03-28 19:40:09 +0300

    1 version

--- src/main/java/boot/RestController.java
+++ src/main/java/boot/RestController.java
@@ -1,8 +1,11 @@
 package boot;
 
+import db.ConnectDb;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.SQLException;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -26,12 +29,19 @@ public class RestController {
         public void setP1(String p1) {
             this.p1 = p1;
         }
+
     }
-    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse restMethod(String name) {
+    @RequestMapping(value = "/delete", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse restMethod(String phone) throws SQLException {
         RestResponse result = new RestResponse();
-        result.setP1("hello");
-        result.setP2(name);
+        ConnectDb db = new ConnectDb();
+        db.connectSql();
+        db.setBlockime(phone);
+        db.deleteSession(phone);
+        db.disconnectSql();
+        result.setP1("удален");
+        result.setP2(phone);
+
 
         return result;
     }
--- src/main/java/db/ConnectDb.java
+++ src/main/java/db/ConnectDb.java
@@ -1,12 +1,59 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ConnectDb {
-    private static Connection connection;
+    static final String DB_URL_SQL = "jdbc:mysql://localhost:3306/Cis";
+    static final String login = "root";
+    static final String password = "450125Ad";
+
+    private static Connection connectionSql;
+    private static Statement statementSql;
     private static PreparedStatement preparedStatement;
-    private static Statement statement;
-    static final String DB_URL = "jdbc:pg01.lt.gismt.crpt.tech";
+
+
+
+    public static void disconnectSql(){
+        try {
+            connectionSql.close();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void connectSql() {
+        try {
+            // Class.forName("com.mysql.jdbc.Driver");
+            connectionSql = DriverManager.getConnection(DB_URL_SQL,login,password);
+            statementSql = connectionSql.createStatement();
+
+
+        } catch (SQLException  e) {
+            e.printStackTrace();
+        }
+
+    }
+
+        public static void setBlockime(String phone) throws SQLException {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            java.util.Date date = new Date();
+            //statementSql = connectionSql.prepareStatement("UPDATE `cis`.`users` SET `blocked` = '" +dateFormat.format(date) + "'WHERE (`phone` = '" + phone+"');");
+            statementSql.execute("UPDATE `cis`.`users` SET `blocked` = '" +dateFormat.format(date) + "'WHERE (`phone` = '" + phone+"');");
+        }
+
+        public static void deleteSession(String phone) throws SQLException {
+        statementSql.execute("Delete from `cis`.`session` where `phone` = '" + phone +"';");
+        }
+
+    public static void main(String[] args) throws SQLException {
+        connectSql();
+        setBlockime("124");
+        deleteSession("123");
+        disconnectSql();
+    }
 }
+
+
